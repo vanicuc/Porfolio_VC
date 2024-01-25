5dade2fb98e4f51e48fc621f079e7a96269134a0
vanicuc <vanicuc@gmail.com>
2024-01-26 00:42:56 +0100

    studios

--- client/src/pages/Educacion.jsx
+++ client/src/pages/Educacion.jsx
@@ -1,23 +1,66 @@
+
+import './Educacion.css';
+
 export default function Educacion() {
+
+ 
+ 
     return (
       <>
       
       <div>
       <h3>Educación:</h3>
+      </div>
 
-      <h4>full stack developer</h4>
-      <p>2023</p>
-      <p>CodeOp</p>
 
-      <h4>UI/UX Deseñador</h4>
-      <p>2023</p>
-      <p>Coderhouse</p>
-
-      <h4>Arquitectura</h4>
-      <p>(1999 - 2006)</p>
+    <body>
+    <div className="container">
+        <div className="cardStudio">
+            <div className="face face1">
+                <div className="content">
+                    {/* <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true"> */}
+                    <h3>full stack developer</h3>
+                </div>
+            </div>
+            <div className="face face2">
+                <div class="content">
+                    <p>2023</p>
+                    <p>CodeOp</p>
+                        {/* <a href="#">Read More</a> */}
+                </div>
+            </div>
+        </div>
+        <div className="cardStudio">
+            <div className="face face1">
+                <div className="content">
+                    {/* <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/code_128.png?raw=true"> */}
+                    <h3>Design UI/UX </h3>
+                </div>
+            </div>
+            <div className="face face2">
+                <div className="content">
+                <p>2023</p>
+                <p>Coderhouse</p>
+                </div>
+            </div>
+        </div>
+        <div className="cardStudio">
+            <div className="face face1">
+                <div className="content">
+                    {/* <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/launch_128.png?raw=true"> */}
+                    <h3>Arquitectura</h3>
+                </div>
+            </div>
+            <div className="face face2">
+                <div className="content">
+                <p>(1999 - 2006)</p>
       <p>Facultad de Arquitectura, Diseño y Urbanismo, universidad Nacional Córdoba</p>
-
+                </div>
+            </div>
+        </div>
     </div>
+</body>
+
 
     <div>
     <h3>Idioma:</h3>
@@ -25,7 +68,6 @@ export default function Educacion() {
     <p>Ingles : Nivel Avanzado</p>
 
     </div>
-
     
   </>
     )

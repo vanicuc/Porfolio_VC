15ec214cffa5cf18bae18eb722062b4e34deaf76
vanicuc <vanicuc@gmail.com>
2024-01-29 14:45:01 +0100

    cards

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import './Projects.css';
 
 export default function Project() {
     return (
@@ -10,7 +11,7 @@ export default function Project() {
   <div className='Experience'>
       <p>
       10+ years work experience in renowned companies in the field of architecture, contributing to notable projects such as the Estel Building and Corte Inglés of Francesc Macia, in parallel to 5+ years experience as freelance professional in small-scale renovations, focusing on functional design.
-My expertise in architecture encompasses a range of skills, including architectural design, project management, BIM modeling, Revit, AutoCAD, among others. I also have teaching experience as an adjunct professor at the National University of Córdoba, Argentina, where I taught the Architecture III undergraduate course.
+      My expertise in architecture encompasses a range of skills, including architectural design, project management, BIM modeling, Revit, AutoCAD, among others. I also have teaching experience as an adjunct professor at the National University of Córdoba, Argentina, where I taught the Architecture III undergraduate course.
       </p>
     </div>
   

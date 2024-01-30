ed8f4d06e08ee70ee3618efdd451ed8bc005e952
vanicuc <vanicuc@gmail.com>
2024-01-30 11:53:29 +0100

    formato

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,47 @@
-import React from 'react';
+import React from "react";
 import { Route, Routes, BrowserRouter, Link, Outlet } from "react-router-dom";
-import Home from './pages/Home';
-import Contacto from './pages/Contacto';
-import OliVino from './pages/OliVino';
-import PadresEnsync from './pages/PadresEnsync';
-import Biker from './pages/Biker';
-import ROLLXPerience from './pages/ROLLXPerience';
-import Habilidad from './pages/habilidad';
-import Educacion from './pages/Educacion';
-import Projects from './pages/Projects';
+import Home from "./pages/Home";
+import Contacto from "./pages/Contacto";
+import OliVino from "./pages/OliVino";
+import PadresEnsync from "./pages/PadresEnsync";
+import Biker from "./pages/Biker";
+import ROLLXPerience from "./pages/ROLLXPerience";
+import Habilidad from "./pages/habilidad";
+import Educacion from "./pages/Educacion";
+import Projects from "./pages/Projects";
 // import AboutMe from './pages/AboutMe';
-
-
 
 function App() {
   return (
     <>
-     <nav className="navbar">
+      <nav className="navbar">
         <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/Habilidad">Skill</Link></li>
-        <li><Link to="/Educacion">Education</Link></li>
-        <li><Link to="/Contacto">Contact</Link></li>
-     </ul>
-      
-    </nav>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Habilidad">Skill</Link>
+          </li>
+          <li>
+            <Link to="/Educacion">Education</Link>
+          </li>
+          <li>
+            <Link to="/Contacto">Contact</Link>
+          </li>
+        </ul>
+      </nav>
 
-       <Routes>
+      <Routes>
         <Route path="/" element={<Home />} /> {/* Cambia "/Home" a "/" */}
         <Route path="/Contacto" element={<Contacto />} />
-        <Route path="/OliVino" element={<OliVino/>} />
-        <Route path="/PadresEnsync" element={<PadresEnsync/>} />
-        <Route path="/Biker" element={<Biker/>} />
-        <Route path="/ROLLXPerience" element={<ROLLXPerience/>} />
-        <Route path="/Habilidad" element={<Habilidad/>} />
-        <Route path="/Educacion" element={<Educacion/>} />
-        <Route path="/Projects" element={<Projects/>} />
+        <Route path="/OliVino" element={<OliVino />} />
+        <Route path="/PadresEnsync" element={<PadresEnsync />} />
+        <Route path="/Biker" element={<Biker />} />
+        <Route path="/ROLLXPerience" element={<ROLLXPerience />} />
+        <Route path="/Habilidad" element={<Habilidad />} />
+        <Route path="/Educacion" element={<Educacion />} />
+        <Route path="/Projects" element={<Projects />} />
         {/* <Route path="/AboutMe" element={<AboutMe/>} /> */}
-
-
       </Routes>
     </>
   );
--- client/src/pages/Contacto.jsx
+++ client/src/pages/Contacto.jsx
@@ -1,22 +1,20 @@
-
-import React, { useState } from 'react';
-import './Contacto.css';
+import React, { useState } from "react";
+import "./Contacto.css";
 
 export default function Contacto() {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    phone: '',
-    message: '',
-    
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
   });
 
   const handleSubmit = (event) => {
     event.preventDefault();
     // manejar la lógica para enviar el formulario, por ejemplo, enviar un correo electrónico o almacenar los datos.
-    console.log('Formulario enviado:', formData);
+    console.log("Formulario enviado:", formData);
     // resetear el estado del formulario después de manejar los datos si es necesario.
-    setFormData({ name: '', email: '',phone: '', message: '' });
+    setFormData({ name: "", email: "", phone: "", message: "" });
   };
 
   const handleInputChange = (event) => {
@@ -24,21 +22,25 @@ export default function Contacto() {
     setFormData({ ...formData, [name]: value });
   };
 
-
-
   return (
     <>
-
-<section className="form_wrap">
+      <section className="form_wrap">
         <section className="cantact_info">
           <section className="info_title">
             <span className="fa fa-user-circle"></span>
             <h2>For More Details Contact Me!</h2>
           </section>
           <section className="info_items">
-            <p><span className="fa-mobile"></span>Thank you for visiting my portfolio</p>
-            <p><span className="fa-mobile"></span>I'm excited to connect with you. If you are interested in discussing potential projects, creative collaborations, or if you are looking for new talents for your team,
-                 I would love to hear from you!</p>
+            <p>
+              <span className="fa-mobile"></span>Thank you for visiting my
+              portfolio
+            </p>
+            <p>
+              <span className="fa-mobile"></span>I'm excited to connect with
+              you. If you are interested in discussing potential projects,
+              creative collaborations, or if you are looking for new talents for
+              your team, I would love to hear from you!
+            </p>
           </section>
         </section>
 
@@ -46,56 +48,49 @@ export default function Contacto() {
           <h2>Send a message</h2>
           <div className="user_info">
             <label htmlFor="name">Name *</label>
-            <input 
-            type="text" 
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleInputChange}
-            required
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+              required
             />
 
             <label htmlFor="phone">Mobile</label>
             <input
-            type="text" 
-            id="phone"
-            name="phone"
-            value={formData.phone}
-            onChange={handleInputChange}
-            required
-            
+              type="text"
+              id="phone"
+              name="phone"
+              value={formData.phone}
+              onChange={handleInputChange}
+              required
             />
 
             <label htmlFor="email">Email *</label>
-            <input 
-            type="text" 
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-            required
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
             />
 
             <label htmlFor="mensaje">Message *</label>
-            <textarea id="message"
-            name="message"
-            value={formData.message}
-            onChange={handleInputChange}
-            required></textarea>
-             <input type="submit" value="Submit" id="btnSend"/>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleInputChange}
+              required
+            ></textarea>
+            <input type="submit" value="Submit" id="btnSend" />
 
             {/* <input type="button" value="Submit" id="btnSend"/> */}
           </div>
         </form>
       </section>
-
     </>
   );
 }
-
-
-
-
-
-
-

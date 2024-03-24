f02e0c07f41df1f47432431f82969ecbdf9820e9
vanicuc <vanicuc@gmail.com>
2024-03-24 18:58:28 +0100

    puerto 5001

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,30 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: "http://localhost:5000",
-//         changeOrigin: true,
-//         secure: false
-//       },
-//       "/img": {
-//         target: "http://localhost:5000",
-       
-//       },
-//     },
-//   },
-// })
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:5000"
-    }
-  }
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false
+      },
+      "/img": {
+        target: "http://localhost:5000",
+       
+      },
+    },
+  },
 })
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": "http://localhost:5000"
+//     }
+//   }
+// })
 
 
 

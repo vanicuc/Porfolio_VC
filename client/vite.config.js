68df58cd25d740bf7ffd680609f5f0ea071af3ab
vanicuc <vanicuc@gmail.com>
2024-03-25 15:35:33 +0100

    netlify command CI=falso npm run build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// export default defineConfig({
+  
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:5000",
+//         changeOrigin: true,
+//         secure: false
+//       },
+//       "/img": {
+//         target: "http://localhost:5000",
+//       },
+//     },
+//   },
+// })
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        secure: false
-      },
-      "/img": {
-        target: "http://localhost:5000",
-      },
-    },
-  },
 })
+
+
+
+
+
+
+
 // export default defineConfig({
 //   plugins: [react()],
 //   server: {
@@ -26,15 +39,3 @@ export default defineConfig({
 // })
 
 
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import viteTsconfigPaths from 'vite-tsconfig-paths';
-// import svgrPlugin from 'vite-plugin-svgr';
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react({
-//     jsxRuntime: 'classic' // Add this line
-//   }), viteTsconfigPaths(), svgrPlugin()],
-// });

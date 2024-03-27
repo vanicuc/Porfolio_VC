672c4ef26121204ace8c8a8955b0767198369d47
vanicuc <vanicuc@gmail.com>
2024-03-27 02:15:59 +0100

    rollxperience figma

--- client/src/pages/ROLLXPerience.jsx
+++ client/src/pages/ROLLXPerience.jsx
@@ -8,6 +8,21 @@ export default function ROLLXPerience() {
         Narrativa, Mi MVP, Arquitectura de la información, Flujo de usuario,
         Moodboard, Ui Kits, Diseño de iconos, Pruebas de usabilidad, Evolución.
       </p>
+
+     
+
+
+{/* <iframe style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }} width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FdoQANdez1wcDeq5xGVnenX%2FWireframe-digital%3Fpage-id%3D1182%253A9055%26type%3Ddesign%26node-id%3D1182-10580%26viewport%3D667%252C293%252C0.07%26t%3Dtbpp1T7QHuPdAdze-1%26scaling%3Dscale-down%26starting-point-node-id%3D1182%253A10580%26mode%3Ddesign" allowFullScreen></iframe> */}
+
+<iframe 
+  style={{ border: '1px solid rgba(0, 0, 0, 0.1)', maxWidth: '800px', width: '100%' }} 
+  height="450" 
+  src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FdoQANdez1wcDeq5xGVnenX%2FWireframe-digital%3Fpage-id%3D1182%253A9055%26type%3Ddesign%26node-id%3D1182-10580%26viewport%3D667%252C293%252C0.07%26t%3Dtbpp1T7QHuPdAdze-1%26scaling%3Dscale-down%26starting-point-node-id%3D1182%253A10580%26mode%3Ddesign" 
+  allowFullScreen
+></iframe>
+
+
+
     </>
   );
 }

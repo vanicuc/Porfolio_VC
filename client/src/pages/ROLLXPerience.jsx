0db7bed1939769735d7decdf778ea540adbbc424
vanicuc <vanicuc@gmail.com>
2024-03-28 01:48:51 +0100

    roll

--- client/src/pages/ROLLXPerience.jsx
+++ client/src/pages/ROLLXPerience.jsx
@@ -84,7 +84,84 @@ export default function ROLLXPerience() {
         </div>
       </section>
 
+      <div className="box_storytell_roll">
+        <h2>To tell through a fictional story </h2>
+        <p>          
+            It's time to dive into the exciting story of Robert, a skater in search of true liberation on wheels! It all started one weekend when he decided to accompany his son Martín in his passion for skating. He remembered how he used to skate in his childhood, so he felt confident!
+
+            With enthusiasm, he put on his skates, but something unexpected happened. While trying to brake, he overshot and lost his balance! What a surprise! That incident left a mark on his confidence, and fear took over. Does that sound familiar to you?
+
+            But Robert didn't give up. Despite the fear, his love for skating was so strong that he sought to overcome himself. He longed to feel the thrill of gliding on the asphalt again and enjoying every turn.
+
+            That's when he found our app, ROLLXPerience. The tutorials, detailed and engaging, guided him step by step. And yes, he even laughed remembering that funny fall!
+
+            With our app, Robert felt accompanied and supported by a community of passionate skaters. Together, they shared stories, routes, and tips. Motivation grew, and Robert challenged himself at every step.
+
+            Now, Robert shines with confidence on wheels. He has overcome his fears and rediscovered the thrill of skating.
+
+            Are you ready to feel that same liberation?
+        </p>
+      </div>
+
      
+
+
+
+
+
+             <section className="section_mvp_biker">   
+              <div className="mvp_container_biker">
+                <div className="mvp-biker">
+                  <h3 className="titulo_section_mvp-biker">MVP 1.2</h3>
+                  <div class="img-container">
+                    <img src="roll.png" alt="foto familia" className="img-mvp-biker" 
+                    style={{ width: "50%", height: "auto" }}/>
+                    
+                    <div class="text-overlay">
+                      <h4>Routes:</h4>
+                      <ul class="box-explication-mvp_biker">
+                      <li>Popular skating locations such as skate parks, parks, trails, and routes.
+                      Save your routes with photos of the moment.
+                      Routes categorized by skating levels.</li>
+                      </ul>
+                      
+                        <h4>Teaching:</h4>
+                        <ul class="box-explication-mvp_biker">
+                          <li>Skating tips and tutorials: graphics and videos
+                          Divided by learning levels.</li>
+                        </ul>
+                     
+                      
+                        <h4>Community:</h4>
+                        <ul class="box-explication-mvp_biker">
+                          <li>Include an online community for users to share skating photos and videos.
+                          Chat and share location with skating contacts.
+                          Events and competitions happening in the user's area, so they can participate or attend</li>
+                        </ul>
+
+                        <h4>Profile:</h4>
+                        <ul class="box-explication-mvp_biker">
+                          <li>Progress tracking: The app could have a feature allowing users to track:
+                          Skating statistics,
+                          Amount of kilometers they have skated,
+                          Connect with other devices like smartwatches, tablets, etc.</li>
+                        </ul>
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+
+                {/* 
+                <img
+                  src="roll_varias pantallas.png"
+                  alt="Roll"
+                  style={{ width: "100%", height: "10" }}
+                   /> */}
+
+            
+
 
 
           {/* <iframe style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }} width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FdoQANdez1wcDeq5xGVnenX%2FWireframe-digital%3Fpage-id%3D1182%253A9055%26type%3Ddesign%26node-id%3D1182-10580%26viewport%3D667%252C293%252C0.07%26t%3Dtbpp1T7QHuPdAdze-1%26scaling%3Dscale-down%26starting-point-node-id%3D1182%253A10580%26mode%3Ddesign" allowFullScreen></iframe> */}
@@ -99,6 +176,15 @@ export default function ROLLXPerience() {
             ></iframe>
 
              </div>
+
+
+
+                 
+            <img
+                  src="UI kits.png"
+                  alt="Roll"
+                  style={{ width: "100%", height: "10" }}
+                />
 
 
 
